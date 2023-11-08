3eb8398818263e24682ad539b5e49cf2e408a681
evansochieng <evansochieng121@gmail.com>
2023-11-08 20:10:03 +0300

    attempt first bit of string matching

--- lib/regex.py
+++ lib/regex.py
@@ -3,7 +3,8 @@ import re
 # NOTE: There are only a few tests included, so multiple solutions will work.
 # Feel free to encourage students to find oversights and add tests to this lab!
 
-name = r""
+# name = r"^[A-Z][\S]+\s?[\w-]*"
+name = r"^[A-Z][A-Za-z\W]+\s?[A-z-]*"
 name_regex = re.compile(name)
 
 phone_number = r""
