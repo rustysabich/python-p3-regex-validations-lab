9140c654fcae1529971df44ab5ede8159836d9ff
evansochieng <evansochieng121@gmail.com>
2023-11-09 07:17:53 +0300

    crack singke space matching

--- lib/regex.py
+++ lib/regex.py
@@ -3,8 +3,7 @@ import re
 # NOTE: There are only a few tests included, so multiple solutions will work.
 # Feel free to encourage students to find oversights and add tests to this lab!
 
-# name = r"^[A-Z][\S]+\s?[\w-]*"
-name = r"^[A-Z][A-Za-z\W]+\s?[A-z-]*"
+name = r"^[A-Z][A-z\']+\s?[A-z-\']*$"
 name_regex = re.compile(name)
 
 phone_number = r""
